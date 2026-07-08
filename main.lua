8569e4866cc824c57c9ca3fa5174ac4957a43371
Lqwrence16 <lqwrencemontqnte@gmail.com>
2026-07-08 18:46:54 +0800

    reverted

--- main.lua
+++ main.lua
@@ -92,7 +92,7 @@ local Library =
 -- WINDOW SETUP
 -- ==============================================================================
 local Window = Library:CreateWindow({
-	Title = "too close",
+	Title = "revert",
 	Footer = "v2.5.0",
 	Icon = "fan",
 	IconSize = UDim2.fromOffset(28, 28),
